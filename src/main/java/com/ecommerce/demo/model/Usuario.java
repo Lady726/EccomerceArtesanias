62ce9be2acf8d035067c006ad53a0cc51313d43c
danielolan <da-olano@hotmail.com>
2023-11-15 12:20:38 -0500

    Subida inicial

---
+++ src/main/java/com/ecommerce/demo/model/Usuario.java
@@ -0,0 +1,19 @@
+package com.ecommerce.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    // Getters y setters...
+}
+
+
+
