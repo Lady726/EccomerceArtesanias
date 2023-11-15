e4c0f125b5cd8fd89849a5baba022a6f159902a4
Lady726 <ljbarrios52@ucatolica.edu.co>
2023-11-15 18:30:11 -0500

    la clase usuario que representa un usuario en el sistema

--- src/main/java/com/ecommerce/demo/model/User.java
+++ src/main/java/com/ecommerce/demo/model/User.java
@@ -12,6 +12,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
+// La clase User que representa un usuario en el sistema
 @Entity
 @Table(name = "users")
 @AllArgsConstructor
