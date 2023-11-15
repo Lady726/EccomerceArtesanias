d4ed8218706319e1311bc54e5595cfd3b3994cb1
Lady726 <ljbarrios52@ucatolica.edu.co>
2023-11-15 18:50:29 -0500

    Clase Principal spring boot

--- src/main/java/com/ecommerce/demo/EcommerceJpaApplication.java
+++ src/main/java/com/ecommerce/demo/EcommerceJpaApplication.java
@@ -3,6 +3,7 @@ package com.ecommerce.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Anotación que marca la clase principal como una aplicación Spring Boot
 @SpringBootApplication
 public class EcommerceJpaApplication {
 
