502aae2489481589ca9ee2c27e9a07e614af303a
Lady726 <ljbarrios52@ucatolica.edu.co>
2023-11-15 18:40:49 -0500

    buscar un usuario por su nombre, correo electronico especifico

--- src/main/java/com/ecommerce/demo/repository/UserRepository.java
+++ src/main/java/com/ecommerce/demo/repository/UserRepository.java
@@ -7,7 +7,10 @@ import com.ecommerce.demo.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+  // Método para buscar un usuario por su nombre
   User findByName(String name);
+  // Método para buscar un usuario por su correo electrónico
   User findByEmail(String email);
+  // Método para verificar si existe un usuario con un correo electrónico específico
   boolean existsByEmail(String email);
 }
